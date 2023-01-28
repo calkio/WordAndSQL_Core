18d899e5a53038ee41ff124ade2d30def13c1613
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-01-28 14:08:11 +0700

    Проверка привязки ViewModel к разметки

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordAndSQL_Core.ViewModels.Base;
+
+namespace WordAndSQL_Core.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Статистика";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
