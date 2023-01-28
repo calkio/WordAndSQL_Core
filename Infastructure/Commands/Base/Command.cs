5407baa3c45ef80bd9fbf292a2182f3c1aaacad9
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-01-28 14:15:43 +0700

    Создание лямда команд

--- Infastructure/Commands/Base/Command.cs
+++ Infastructure/Commands/Base/Command.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WordAndSQL_Core.Infastructure.Commands.Base
 {
-    class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
