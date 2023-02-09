881c1b9ef51925936cc3b0de32aacdddb05c1da7
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-02-09 11:51:13 +0700

    Try catch на соединение с базой

--- ViewModels/UserPassportViewModel.cs
+++ ViewModels/UserPassportViewModel.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Windows;
 using WordAndSQL_Core.Entity;
 using WordAndSQL_Core.Views.Windows;
 
@@ -10,7 +11,7 @@ namespace WordAndSQL_Core.ViewModels
 {
     class UserPassportViewModel
     {
-        string sqlConnection = "Data Source=CALKIO\\MSSQLSERVER01;Initial Catalog=WordAndSQL;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string sqlConnection = "Data Source=CALKIO\\;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         #region Данныe о пользователи
 
@@ -20,14 +21,24 @@ namespace WordAndSQL_Core.ViewModels
         /// <returns></returns>
         public Users GetDataUsers()
         {
-            using (var connection = new SqlConnection(sqlConnection))
+            try
             {
-                var sql = @"SELECT * FROM Users WHERE id=2";
+                using (var connection = new SqlConnection(sqlConnection))
+                {
+                    var sql = @"SELECT * FROM Users WHERE id=2";
 
-                var user = connection.QuerySingle<Users>(sql);
+                    var user = connection.QuerySingle<Users>(sql);
 
-                return user;
+                    return user;
+                }
             }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных", "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                return new Users();
+            }
+            
         }
 
         #endregion
