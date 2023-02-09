df88c0eac7ed5e6dc361601610b4e874635e6625
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-02-09 16:03:40 +0700

    Стиль для DataGrid

--- ViewModels/UserPassportViewModel.cs
+++ ViewModels/UserPassportViewModel.cs
@@ -11,7 +11,7 @@ namespace WordAndSQL_Core.ViewModels
 {
     class UserPassportViewModel
     {
-        string sqlConnection = "Data Source=CALKIO\\;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string sqlConnection = "Data Source=CALKIO\\MSSQLSERVER01;Initial Catalog=WordAndSQL;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         #region Данныe о пользователи
 
@@ -34,7 +34,7 @@ namespace WordAndSQL_Core.ViewModels
             }
             catch (System.Exception)
             {
-                MessageBox.Show("Ошибка подключения к базе данных", "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MessageBox.Show("Ошибка подключения к базе данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
 
                 return new Users();
             }
@@ -51,13 +51,22 @@ namespace WordAndSQL_Core.ViewModels
         /// <returns></returns>
         public List<Users> GetDataGroup()
         {
-            using (var connection = new SqlConnection(sqlConnection))
+            try
             {
-                var sql = @"SELECT * FROM Users WHERE id=2";
+                using (var connection = new SqlConnection(sqlConnection))
+                {
+                    var sql = @"SELECT * FROM Users";
 
-                var users = connection.Query<Users>(sql).ToList();
+                    var users = connection.Query<Users>(sql).ToList();
 
-                return users;
+                    return users;
+                }
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                return new List<Users>();
             }
         }
 
@@ -70,11 +79,18 @@ namespace WordAndSQL_Core.ViewModels
         /// </summary>
         public void SaveTextBox(string SecondName, string FirstName, string Surname, string Numder, string BirthDate, string Telephone, string Login, string Snils, string Citizenship, string Gender)
         {
-            using (var connection = new SqlConnection(sqlConnection))
+            try
             {
-                var sql = $"UPDATE Users SET SecondName='{SecondName}', FirstName='{FirstName}', Surname='{Surname}', Numder='{Numder}', BirthDate='{BirthDate}', Telephone='{Telephone}', Login='{Login}', Snils='{Snils}', Citizenship='{Citizenship}', Gender='{Gender}' WHERE id=2";
+                using (var connection = new SqlConnection(sqlConnection))
+                {
+                    var sql = $"UPDATE Users SET SecondName='{SecondName}', FirstName='{FirstName}', Surname='{Surname}', Numder='{Numder}', BirthDate='{BirthDate}', Telephone='{Telephone}', Login='{Login}', Snils='{Snils}', Citizenship='{Citizenship}', Gender='{Gender}' WHERE id=2";
 
-                var users = connection.Query(sql);
+                    var users = connection.Query(sql);
+                }
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
@@ -83,11 +99,18 @@ namespace WordAndSQL_Core.ViewModels
         /// </summary>
         public void SaveNotes(string PlaceWork, string Post, string Education, string Comment)
         {
-            using (var connection = new SqlConnection(sqlConnection))
+            try
             {
-                var sql = $"UPDATE Users SET PlaceWork='{PlaceWork}', Post='{Post}', Education='{Education}', Comment='{Comment}' WHERE id=2";
+                using (var connection = new SqlConnection(sqlConnection))
+                {
+                    var sql = $"UPDATE Users SET PlaceWork='{PlaceWork}', Post='{Post}', Education='{Education}', Comment='{Comment}' WHERE id=2";
 
-                var users = connection.Query(sql);
+                    var users = connection.Query(sql);
+                }
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
