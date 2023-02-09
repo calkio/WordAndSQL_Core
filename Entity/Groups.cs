8d3d288f6ece9ffca1de8f068cd50f82a29138a5
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-02-09 16:13:05 +0700

    Создание таблицы групп в базе + сущности групп

---
+++ Entity/Groups.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordAndSQL_Core.Entity
+{
+    class Groups
+    {
+        public int id { get; set; }
+        public string idUser { get; set; }
+        public string Name { get; set; }
+        public string Numder { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
