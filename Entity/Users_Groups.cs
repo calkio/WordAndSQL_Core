0f0d6c42e80610256fa5f6aba663b6c504f2bf2f
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-02-23 16:08:58 +0700

    Создание сущности таблицы связывающая пользователей и группы

---
+++ Entity/Users_Groups.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordAndSQL_Core.Entity
+{
+    internal class Users_Groups
+    {
+    }
+}
